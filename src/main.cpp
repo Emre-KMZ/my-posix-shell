18e47c78fda58cbfd9a849bacda31a96d735c731
Emre Kirmizi <ex4ux1@gmail.com>
2025-02-09 18:58:27 +0300

    kind of handled the quotes

--- src/main.cpp
+++ src/main.cpp
@@ -5,8 +5,38 @@
 #include "CommandManager.hpp"
 #include <filesystem>
 
-
 CommandManager globalCommandManager; 
+
+void extractArgs(const std::string& input, std::vector<std::string>& args) {
+  // TODO: Un-closed quotes are not handled, yet
+
+  std::string arg = "";
+  bool inDoubleQuote = false;
+  bool inSingleQuote = false;
+
+  for (size_t i = 0; i < input.size(); i++) {
+    if (input[i] == ' ' && !inDoubleQuote && !inSingleQuote) {
+      if (arg != "") {
+        args.push_back(arg);
+        arg = "";
+      }
+    } else if (input[i] == '\"' && !inSingleQuote) {
+      inDoubleQuote = !inDoubleQuote;
+    } else if (input[i] == '\'' && !inDoubleQuote) {
+      inSingleQuote = !inSingleQuote;
+    } else {
+      if (input[i] == '\\' || (input[i + 1] == '\"' || input[i + 1] == (char)96 || input[i + 1] == '$')) {
+        arg += input[i + 1];
+        i++;
+      } else {
+        arg += input[i];
+      }
+    }
+  }
+  if (arg != "") {
+    args.push_back(arg);
+  }
+}
 
 int main() {
   // flush after every std::cout / std:cerr
@@ -24,13 +54,8 @@ int main() {
     std::getline(std::cin, input);
     
     args.clear();
-    
-    std::istringstream ss(input);
 
-    // parse input into command and arguments
-    while (ss >> arg) {
-      args.push_back(arg);
-    }
+    extractArgs(input, args);
 
     exitCode = globalCommandManager.executeCommand(args);
     if (args[0] == "exit") {
